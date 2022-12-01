7775a6332c1ae2485a59cdff9edd2c1da6308281
Anthony Beasley <anthony@The-Hidden-Genius-Project-87.local>
2022-11-30 18:23:11 -0800

    first commit

---
+++ 1-output.py
@@ -0,0 +1,4 @@
+#print() - Print function
+#string Concatenation - combine 2 or more strings together with (+) sign 
+#input() - Function get information from user
+input(" What is your name?")
---
+++ 3-input.py
@@ -0,0 +1,4 @@
+#print() - Print function
+#string Concatenation - combine 2 or more strings together with (+) sign 
+#input() - Function get information from userinput(" What is your name?")
+print("Hello " + input(" What is your name?"))
